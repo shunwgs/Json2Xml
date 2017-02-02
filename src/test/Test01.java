53daa4dbc47516535be0a3897aeebc1801d7899e
wgs <wgs@wgs-PC>
2017-02-02 17:51:41 +0800

    test

---
+++ src/test/Test01.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        
+        String num = "2";
+        switch(num){
+        case "1":System.out.println(" I am one");
+        break;
+        case "2":System.out.println("second");
+        
+        }
+    }
+}
